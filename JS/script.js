92a260fdca6bc30f3aa40ef35a664848b3cf077a
FernandezJeune <44892189+FernandezJeune@users.noreply.github.com>
2021-05-27 17:00:42 +0800

    Jeune

--- JS/script.js
+++ JS/script.js
@@ -2,21 +2,42 @@ let weather = {
 	"apiKey":"2c247d35d3bdfb2676f0c12c7e2bdd36", 
 	fetchWeather: function(city) {
 		fetch(
-			"http://api.openweathermap.org/data/2.5/weather?q=" 
-			+ city 
-			+ "&units=metric&appid="
-			+ this.apiKey
+			"http://api.openweathermap.org/data/2.5/weather?q=" + 
+			city + 
+			"&units=metric&appid=" + 
+			this.apiKey
 		)
-		.then((response) => response.json())
-		.then((data) => this.displayWeather(data));
+			.then((response) => response.json())
+			.then((data) => this.displayWeather(data));
 	},
 	displayWeather: function(data) {
 		const { name } = data;
 		const { icon,description } = data.weather[0];
 		const { temp,humidity } = data.main;
 		const { speed } = data.wind;
-		console.log(name,icon,description,temp,humidity,speed);
+		console.log(name, icon, description, temp, humidity, speed);
 		document.querySelector(".city").innerText = "Weather in " + name;
-		document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+		document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+		document.querySelector(".description").innerText = description;
+		document.querySelector(".temp").innerText = temp + "°C";
+		document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+		document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+	},
+	search: function(){
+		this.fetchWeather(document.querySelector(".search-bar").value);
 	}
 };
+
+document
+	.querySelector(".search button")
+	.addEventListener("click",function (){
+		weather.search();
+	});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (){
+	if(event.key == "Enter"){
+		weather.search();
+	}
+});
+
+weather.fetchWeather("Baguio");
