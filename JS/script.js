e20f420c02ece61927c8dc7c1a1c8ac8faa0a1f6
FernandezJeune <44892189+FernandezJeune@users.noreply.github.com>
2021-05-27 16:06:44 +0800

    Jeune

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,22 @@
 let weather = {
-	"apiKey":"2c247d35d3bdfb2676f0c12c7e2bdd36"
-}
+	"apiKey":"2c247d35d3bdfb2676f0c12c7e2bdd36", 
+	fetchWeather: function(city) {
+		fetch(
+			"http://api.openweathermap.org/data/2.5/weather?q=" 
+			+ city 
+			+ "&units=metric&appid="
+			+ this.apiKey
+		)
+		.then((response) => response.json())
+		.then((data) => this.displayWeather(data));
+	},
+	displayWeather: function(data) {
+		const { name } = data;
+		const { icon,description } = data.weather[0];
+		const { temp,humidity } = data.main;
+		const { speed } = data.wind;
+		console.log(name,icon,description,temp,humidity,speed);
+		document.querySelector(".city").innerText = "Weather in " + name;
+		document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+	}
+};
