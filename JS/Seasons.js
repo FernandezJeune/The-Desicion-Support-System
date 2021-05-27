149c461d494341b0ef2acc9a1d8931c92bfd081b
JP <johnpaul.aben.d@gmail.com>
2021-05-27 15:46:58 +0800

    JP

--- JS/Seasons.js
+++ JS/Seasons.js
@@ -15,8 +15,40 @@ for(var i = 0; i < items.length; i++){
 
 
         if(index == 0  ){
-            console.log("JANUARY si selected");
-            document.getElementsByClassName("YieldPattern").innerHTML="JANUARY";
+            document.getElementById("Info").innerHTML="<h1>January info</h1>";
+        }
+        if(index == 1  ){
+            document.getElementById("Info").innerHTML="<h1>February</h1>";
+        }
+        if(index == 2  ){
+            document.getElementById("Info").innerHTML="<h1>March</h1>";
+        }
+        if(index == 3  ){
+            document.getElementById("Info").innerHTML="<h1>april</h1>";
+        }
+        if(index == 4  ){
+            document.getElementById("Info").innerHTML="<h1>may</h1>";
+        }
+        if(index == 5  ){
+            document.getElementById("Info").innerHTML="<h1>june</h1>";
+        }
+        if(index == 6  ){
+            document.getElementById("Info").innerHTML="<h1>july</h1>";
+        }
+        if(index == 7  ){
+            document.getElementById("Info").innerHTML="<h1>august</h1>";
+        }
+        if(index == 8  ){
+            document.getElementById("Info").innerHTML="<h1>september</h1>";
+        }
+        if(index == 9  ){
+            document.getElementById("Info").innerHTML="<h1>october</h1>";
+        }
+        if(index == 10  ){
+            document.getElementById("Info").innerHTML="<h1>november</h1>";
+        }
+        if(index == 11  ){
+            document.getElementById("Info").innerHTML="<h1>december</h1>";
         }
     };
 }
