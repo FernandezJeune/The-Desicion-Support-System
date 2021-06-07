aad80bcc6295e549cf33b7af44c5249339b305f4
JP <johnpaul.aben.d@gmail.com>
2021-06-07 12:38:06 +0800

    adds

--- JS/js.js
+++ JS/js.js
@@ -15,40 +15,34 @@ for(var i = 0; i < items.length; i++){
 
 
         if(index == 0  ){
-            document.getElementById("VegtableInfo").innerHTML="<h1>January info</h1>";
+            document.getElementById("VegtableInfo").innerHTML="<h1>Cabbage info . . . . </h1>";
         }
         if(index == 1  ){
-            document.getElementById("VegtableInfo").innerHTML="<h1>February info . . . . </h1>";
+            document.getElementById("VegtableInfo").innerHTML="<h1>Potato info . . . . </h1>";
         }
         if(index == 2  ){
-            document.getElementById("VegtableInfo").innerHTML="<h1>March</h1>";
+            document.getElementById("VegtableInfo").innerHTML="<h1>Carrot info . . . . </h1>";
         }
         if(index == 3  ){
-            document.getElementById("VegtableInfo").innerHTML="<h1>april</h1>";
+            document.getElementById("VegtableInfo").innerHTML="<h1>Chinese Cabbage info . . . . </h1>";
         }
         if(index == 4  ){
-            document.getElementById("VegtableInfo").innerHTML="<h1>may</h1>";
+            document.getElementById("VegtableInfo").innerHTML="<h1>Sweat Peas info . . . . </h1>";
         }
         if(index == 5  ){
-            document.getElementById("VegtableInfo").innerHTML="<h1>june</h1>";
+            document.getElementById("VegtableInfo").innerHTML="<h1>Lettuce info . . . . </h1>";
         }
         if(index == 6  ){
-            document.getElementById("VegtableInfo").innerHTML="<h1>july</h1>";
+            document.getElementById("VegtableInfo").innerHTML="<h1>Bell Pepper info . . . . </h1>";
         }
         if(index == 7  ){
-            document.getElementById("VegtableInfo").innerHTML="<h1>august</h1>";
+            document.getElementById("VegtableInfo").innerHTML="<h1>Broccoli info . . . . </h1>";
         }
         if(index == 8  ){
-            document.getElementById("VegtableInfo").innerHTML="<h1>september</h1>";
+            document.getElementById("VegtableInfo").innerHTML="<h1>Cauli Flower info . . . . </h1>";
         }
         if(index == 9  ){
-            document.getElementById("VegtableInfo").innerHTML="<h1>october</h1>";
-        }
-        if(index == 10  ){
-            document.getElementById("VegtableInfo").innerHTML="<h1>november</h1>";
-        }
-        if(index == 11  ){
-            document.getElementById("VegtableInfo").innerHTML="<h1>december</h1>";
+            document.getElementById("VegtableInfo").innerHTML="<h1>Tomato info . . . . </h1>";
         }
     };
 }
